42f3b5679b6032f6b3ce23650ff90feeb7d5011e
Bryce Guinta <azrathud@gmail.com>
2015-03-14 19:41:18 -0600

    add nearest neighbor test data

---
+++ nearest_neighbor.cpp
@@ -0,0 +1,30 @@
+#include <mlpack/core.hpp> // ParseCommand
+#include <mlpack/methods/neighbor_search/neighbor_search.hpp> // NeighborSearch
+#include <iostream>
+
+using namespace mlpack;
+using namespace mlpack::neighbor;
+using namespace mlpack::metric;
+
+int main(int argc, char** argv) {
+    //CLI::ParseCommandLine(argc, argv);
+
+    // Our dataset matrix, which is column-major
+    arma::mat data;
+    data::Load("data.csv", data, true);
+
+    NeighborSearch<NearestNeighborSort, EuclideanDistance> nn(data);
+
+    arma::Mat<size_t> neighbors;
+    arma::mat distances;
+
+    nn.Search(1, neighbors, distances);
+
+    const int SIZE = data.n_rows;
+    std::cout << SIZE;
+
+    for(size_t i = 0, j = 0; i < neighbors.n_elem; i++, j+=2) {
+    std::cout << "Nearest neighbor of point (" << data[j] << ", " << data[j+1] << ") at " << i << " is point "
+        << neighbors[i] << " at (" << data[neighbors[i] * SIZE] << ", " << data[neighbors[i] * SIZE + 1] <<  ") and the distance is " << distances[i] << ".\n";
+    }
+}
