54940f1b7c4f6db02721a2ca81386a0899850da0
Bryce Guinta <azrathud@gmail.com>
2015-03-21 13:25:34 -0600

    add tut comments to first ex program

--- 2d_1neighbor_nearest_neighbor.cpp
+++ 2d_1neighbor_nearest_neighbor.cpp
@@ -1,29 +1,41 @@
-#include <mlpack/core.hpp> // ParseCommand
+#include <mlpack/core.hpp>
 #include <mlpack/methods/neighbor_search/neighbor_search.hpp> // NeighborSearch
 #include <iostream>
 
-using namespace mlpack;
-using namespace mlpack::neighbor;
-using namespace mlpack::metric;
+// There are two libraries being used
+// thing with the arma:: namespace are from the armadillo library and can be
+// referenced here ( http://arma.sourceforge.net/docs.html )
 
-int main(int argc, char** argv) {
-    // Our dataset matrix, which is column-major
+// Things in the mlpack namespace build upon doing operations upon these
+// Matrices created with the armadillo library
+
+int main() {
+    // Dataset matrix loaded from '2d_data.csv'
     arma::mat data;
-    data::Load("data.csv", data, true);
+    mlpack::data::Load("2d_data.csv", data, true); // "2d_data.csv" the comma separated file from which the matrix is loaded from. The matrix is stored with each line being a colum. data is referenced in the Load function and the matrix will be loaded into that mat object. true means this function failes terminated the program if something goes wrong
 
-    NeighborSearch<NearestNeighborSort, EuclideanDistance> nn(data);
+    mlpack::neighbor::NeighborSearch<mlpack::neighbor::NearestNeighborSort, mlpack::metric::EuclideanDistance> nn(data); // create an object called nn of class NeighborSearch which preforms the nearest neighbor sort and uses euclidean distance(fractional values). The data object is stored in nn
 
-    arma::Mat<size_t> neighbors;
-    arma::mat distances;
+    arma::Mat<size_t> neighbors; // Create a mat object to recieve neighborsfrom search
+    arma::mat distances; // Create a mat object to recieve distances relative to each data point
 
-    nn.Search(1, neighbors, distances);
-    const int SIZE = data.n_rows    ;
+    nn.Search(1, neighbors, distances); // do a nearest neighbor search on the data stored in the mat object 'data'. Only look for the 1st nearest neighbor. Store the nearest neighbor index in the neigbhors mat, and store the distances corresponding to the data matrix in the distances mat
+    const int SIZE = data.n_rows;
 
+    // access the matrices and extract the nearest neighbor and distance assuming a 2d array
+    // Matrices are squashed into a 1d array
     for(size_t i = 0, j = 0; i < neighbors.n_elem; i++, j+=2) {
-    std::cout << "Nearest neighbor of point (" << data[j] << ", " << data[j+1] << ") at " << i << " is point "
-        << neighbors[i] << " at (" << data[neighbors[i] * SIZE] << ", " << data[neighbors[i] * SIZE + 1] <<  ") and the distance is " << distances[i] << ".\n";
+        std::cout 
+        << "Nearest neighbor of point (" 
+            << data[j] << ", " << data[j+1] 
+        << ") at " << i 
+        << " is point " << neighbors[i] 
+        << " at (" 
+        << data[neighbors[i] * SIZE] << ", " << data[neighbors[i] * SIZE + 1] 
+        <<  ") and the distance is " << distances[i] << ".\n";
     }
 
-    data::Save("neighbors.csv", neighbors, true);
-    data::Save("distances.csv", distances, true);
+    // Serialize matrices to files with comma
+    mlpack::data::Save("neighbors.csv", neighbors, true);
+    mlpack::data::Save("distances.csv", distances, true);
 }
