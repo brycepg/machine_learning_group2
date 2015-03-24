d01f5bb9d4ee384ac8d9a6eef2a1109a6156dc6d
Bryce Guinta <azrathud@gmail.com>
2015-03-24 16:09:03 -0600

    generate_plot: compile kmeans if no kmeans.out

--- kmeans_graph/generate_plot.sh
+++ kmeans_graph/generate_plot.sh
@@ -5,12 +5,16 @@
 
 EXEC_FILE="./kmeans.out"
 
+if [ ! -e kmeans.out ]; then
+    ../mlcompile.sh kmeans.cpp
+fi
+
 if (( ${#@} > 1 )); then
      $EXEC_FILE $1 $2
 elif (( ${#@} > 0 )); then
     $EXEC_FILE $1 
 else
-    $EXEC_FILE
+    $EXEC_FILE 10
 fi
 
 gnuplot plot.gp
