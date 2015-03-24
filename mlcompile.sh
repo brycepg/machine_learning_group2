63856932127e09adc2860be09cea009e5482deab
Bryce Guinta <azrathud@gmail.com>
2015-03-24 11:54:52 -0600

    add c++11 mlcompile.sh

--- mlcompile.sh
+++ mlcompile.sh
@@ -2,6 +2,7 @@
 
 # Supply the file as an argument to this script to compile it using mlpack
 # without the need for Makefile installed
+# output is [filename].out
 
-echo "g++ -I/usr/include/libxml2/ $1 -o ${1/.cpp/.out} -lmlpack -Wl,-rpath=/usr/local/lib"
-g++ -I/usr/include/libxml2/ $1 -o ${1/.cpp/.out} -lmlpack -Wl,-rpath=/usr/local/lib
+echo "g++ --std=c++11 -I/usr/include/libxml2/ $1 -o ${1/.cpp/.out} -lmlpack -Wl,-rpath=/usr/local/lib"
+g++ --std=c++11 -I/usr/include/libxml2/ $1 -o ${1/.cpp/.out} -lmlpack -Wl,-rpath=/usr/local/lib
