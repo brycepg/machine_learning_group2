32fab21ef20effdf727efa4681301bdc8f7a763a
Bryce Guinta <azrathud@gmail.com>
2015-04-01 13:46:50 -0600

    add armadillo

--- mlcompile.sh
+++ mlcompile.sh
@@ -4,5 +4,5 @@
 # without the need for Makefile installed
 # output is [filename].out
 
-echo "g++ --std=c++11 -I/usr/include/libxml2/ $1 -o ${1/.cpp/.out} -lmlpack -Wl,-rpath=/usr/local/lib"
+echo "g++ --std=c++11 -I/usr/include/libxml2/ $1 -o ${1/.cpp/.out} -lmlpack -larmadillo -Wl,-rpath=/usr/local/lib"
 g++ --std=c++11 -I/usr/include/libxml2/ $1 -o ${1/.cpp/.out} -lmlpack -Wl,-rpath=/usr/local/lib
